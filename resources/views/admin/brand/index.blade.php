8750137601a93219c948ed13ff3cfad92e63d21e
henry-49 <henryswebdevelopment@gmail.com>
2021-05-26 09:29:47 +0200

    Update User Profile pic using php artisan storage:link

--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
 
-           Hi.. <b></b>
+           All Brands <b></b>
         </h2>
     </x-slot>
 
